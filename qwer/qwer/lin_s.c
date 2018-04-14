8e621db811f3d8beadbc7ecf458136227ad39058
Александр Квадратный <bukass@mail.ru>
2018-04-14 11:58:37 +0300

    parser_ready

---
+++ qwer/qwer/lin_s.c
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+#include "string.h"
+char mass_bis_s[1000][10];
+int bis_max = 0;
+
+//Получаем номер протокола
+int get_bis_type(char *s){
+	for (int i = 0; i < bis_max;i++)
+	if (!strcmp(s, mass_bis_s[i]))
+		return i;
+	strcpy(mass_bis_s[bis_max], s);
+	return bis_max++;
+}
--- qwer/qwer/qwer.c
+++ qwer/qwer/qwer.c
@@ -13,52 +13,126 @@ char s[1000];
 
 //Структура одного пакета
 struct packet{
-	long time;
+	long long int time;
 	int ip;
+	int my_ip;
 	int rr;
-	int section;
-	char protocol;
+	//int section;
+	int protocol;
 };
 
-//Ищем клбчевое слово <packet>
+struct packet *global;
+
+//Начальные символы
 void start_search(){
-	char scmp[] = "<packet>00";
-	scmp[8] = 13;
-	scmp[9] = 10;
-	//printf("%c", 10);
+	fgets(s, sizeof(s), zlo);
+	//fgets(s, sizeof(s), zlo);
+	//fgets(s, sizeof(s), zlo);
+	//fgets(s, sizeof(s), zlo);
+	//fgets(s, sizeof(s), zlo);
+}
+
+//Переводим ip в число
+int ip_to_int(char*s){
+	//printf("\n^^ %s\n", s);
+	int r = 0;
+	int now = 0;
+	int i = 0;
 	do{
-		//system("pause");
-		fgets(s, sizeof(s), zlo);
-		//printf("%s", s);
-		/*for (int i = 0; i < strlen(s); i++)
-			printf("%d)%d %d\n", i, s[i], scmp[i]);*/
-	} while (strcmp(s, scmp));
+		if (s[i] == '.'){
+			r = r * 256 + now;
+			now = 0;
+		}
+		else
+			now = now * 10 + s[i];
+	} while (s[++i] != '\0');
+	r += now;
+	//printf("%u\n", r);
+	return r;
 }
 
 //Извлекаем время
-void parse_time(){
-	char* str = s + 10;
-	str[strlen(str) - 12] = '\0';
-	printf("%s", str);
+void parse_time(char *s){
+	//printf("\t%s", s);
+	int i = 0;
+	int se = 0;
+	int ms = 0;
+	global->time = 0;
+	for (i = 0; i < strlen(s); i++){
+		switch (s[i])
+		{
+		case ':':
+			global->time = global->time *60 + se;
+			se = 0;
+			break;
+		case ',':
+		{
+					global->time = (global->time * 60 + se)*1000000;
+			break;
+		}
+		default:
+			if (i>8)
+				ms = ms * 10 + s[i] - '0';
+			else se = se * 10 + s[i] - '0';
+			break;
+		}
+	}
+	global->time += ms;
+}
+
+//Следующий параметр
+void next_s(int*i, int*i_start){
+	*i += 3;
+	*i_start = *i;
+	while (s[++(*i)] != '"');
+	s[*i] = '\0';
+}
+
+//Вывод пакета
+void print_packet(struct packet* p){
+	printf("\ntime:%lld\nip1:%d\nip2:%d\nprotocol:%d\nlength:%d\n", p->time, p->ip, p->my_ip, p->protocol, p->rr);
 }
 
 //Парсим один пакет
-void parse(){
+int parse(){
+	s[0] = '\0';
 	fgets(s, sizeof(s), zlo);
-	fgets(s, sizeof(s), zlo);
-	printf("%s", s);
-	parse_time();
-	fgets(s, sizeof(s), zlo);
-	fgets(s, sizeof(s), zlo);
-	fgets(s, sizeof(s), zlo);
-	fgets(s, sizeof(s), zlo);
-	fgets(s, sizeof(s), zlo);
+	//printf("%s", s);
+	if (s[0]=='\0') return 0;
+	//printf("%s\n", s);
+	/*Извлечь время*/
+	int i = 0;
+	int u = 3;
+	int i_start;
+	while (u){
+		if (s[i++] == '"')
+			--u;
+	}
+	i_start = i;
+	while (s[++i] != '"');
+	s[i] = '\0';
+	parse_time(s+i_start);
+	next_s(&i, &i_start);
+	//printf("!%s\n", s + i_start);
+	global->ip = ip_to_int(s + i_start);
+	next_s(&i, &i_start);
+	global->my_ip = ip_to_int(s + i_start);
+	next_s(&i, &i_start);
+	global->protocol = get_bis_type(s + i_start);
+	next_s(&i, &i_start);
+	global->rr = atoi(s + i_start);
+	print_packet(global);
+	return 1;
 }
 
 int main(){
+	global = (struct packet*)malloc(sizeof(struct packet));
 	zlo = fopen("test", "rb");
 	start_search();
-	parse();
+	while (parse()){
+		
+	}
+	//printf("\n%lld\n", global->time);
 	fclose(zlo);
 }
 
